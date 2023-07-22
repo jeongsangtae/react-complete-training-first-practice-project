c1007abf75f136b461576770874e17c4e3534ce4
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-22 21:21:10 +0900

    React Complete Training First Practice Project - Card component add

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <lable htmlFor="username">Username</lable>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
